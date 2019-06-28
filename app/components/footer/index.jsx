17c7cd18f0ee68210f11b87277ff80451844c945
Rafael Ceron <rceron@chewy.com>
2019-06-28 13:22:52 -0400

    icones

--- app/components/footer/index.jsx
+++ app/components/footer/index.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import packageJson from '../../../package.json';
 
 import style from './styles.css';
 
 const Footer = () => (
   <footer className={style.footer} >
     <div className={style.author}>rafaelceron.com</div>
-    <div className={style.version}>v0.1.0</div>
+    <div className={style.version}>v{packageJson.version}</div>
   </footer>
 );
 
